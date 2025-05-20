8dfe28785790b2fd1f588ecf929d77e198f81482
Plank Cao <sieglive@gmail.com>
2025-05-20 11:51:42 +0800

    remove debug tracing

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -35,7 +35,7 @@ export const appConfig: ApplicationConfig = {
         provideRouter(
             routes,
             withEnabledBlockingInitialNavigation(),
-            withDebugTracing(),
+            // withDebugTracing(),
             withPreloading(NdcPreloadAllModules),
         ),
         // provideClientHydration(
